28dd45404fa87eb71062b0d6a092b8a3ed73df9c
Damian Markowski <contact@damianmarkowski.pl>
2017-07-27 22:35:54 +0200

    section header cell added

---
+++ Dynamic UITableViewCell/Screens/CitiesList/ViewComponents/CityTableViewSectionHeaderCell.swift
@@ -0,0 +1,13 @@
+//
+//  CityTableViewSectionHeaderCell.swift
+//  Dynamic UITableViewCell
+//
+//  Created by Damian Markowski on 27.07.2017.
+//  Copyright © 2017 Damian Markowski. All rights reserved.
+//
+
+import UIKit
+
+class CityTableViewSectionHeaderCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+}
